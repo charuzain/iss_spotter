3d9222425f146cdce66f269ce714847d2de241d1
charuzain <charuabhizain@gmail.com>
2021-11-11 11:53:44 -0500

    Refactoring

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
 
- fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+/*
+// fetchMyIP((error,ip)=>{
+//   if(error){
+//     console.log("It didn't work" , error);
+//     return;
+//   }
+//   console.log("IP address is" , ip)
+// })
+// fetchCoordsByIP('76.68.24.31',(error,data)=>{
+//   if(error){
+//     console.log("Error in fetching geo-cordinates is",error);
+//     return;
+//   }
+ 
+//   console.log("Cordinates are :", data)
+// })
+
+// const coordinates = {latitude : 43.7806   ,
+//                       longitude: -79.3503 }
+// fetchISSFlyOverTimes(coordinates ,(error,data)=>{
+//   if(error){
+//     console.log("It didn't work" , error);
+//     return;
+//   }
+//   console.log("Flyover time are:",data)
+
+
+// })  */
+
+nextISSTimesForMyLocation((error,passTime)=>{
+  if(error){
+    console.log("It didn't work:",error);
     return;
   }
+  for (const pass of passTime) {
+    const datetime = new Date(0);
+   datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  
+   }
 
-  console.log('It worked! Returned IP:' , ip);
 })
-
-
-
-fetchCoordsByIP('7', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
-    
